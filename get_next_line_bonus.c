c6147f802bd7f5bc5d1a59a5303af7704924abbe
Miguel Barylak alcaraz <mbarylak@c2r6s4.42madrid.com>
2021-10-26 14:03:11 +0200

    added bonuses

---
+++ get_next_line_bonus.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbarylak <mbarylak@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/22 13:54:18 by mbarylak          #+#    #+#             */
+/*   Updated: 2021/10/26 14:02:20 by mbarylak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static char	*ft_free(char **file)
+{
+	if (*file)
+	{
+		free(*file);
+		*file = NULL;
+	}
+	return (NULL);
+}
+
+static char	*get_line(char **file, int fd)
+{
+	char	*aux;
+	char	*line;
+	int		i;
+
+	if (!file[fd])
+		return (NULL);
+	aux = file[fd];
+	i = ft_strchr(aux, '\n');
+	if (i == -1)
+	{
+		if (ft_strlen(aux) <= 0)
+			return (ft_free(file + fd));
+		line = ft_strdup(aux);
+		ft_free(&aux);
+		file[fd] = NULL;
+		return (line);
+	}
+	line = ft_substr(aux, 0, i + 1);
+	file[fd] = ft_substr(aux, i + 1, (ft_strlen(aux) - i));
+	ft_free(&aux);
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	char		buf[BUFFER_SIZE + 1];
+	static char	*file[FD_SIZE];
+	int			ret;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	if (!file[fd])
+		file[fd] = ft_strdup("");
+	ret = 1;
+	while (ret > 0)
+	{
+		if (ft_strchr(file[fd], '\n') == -1)
+		{
+			ret = read(fd, buf, BUFFER_SIZE);
+			if (ret == -1)
+				return (ft_free(file + fd));
+			buf[ret] = '\0';
+			file[fd] = ft_strjoin(file[fd], buf);
+		}
+		else
+			return (get_line(file, fd));
+	}
+	return (get_line(file, fd));
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbarylak <mbarylak@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/22 13:55:10 by mbarylak          #+#    #+#             */
+/*   Updated: 2021/10/26 13:33:30 by mbarylak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+
+# define GET_NEXT_LINE_H
+# define FD_SIZE 1024
+
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+
+int		ft_strlen(char *s);
+int		ft_strchr(char *s, int c);
+char	*ft_strdup(char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_substr(char *s, int start, int len);
+char	*get_next_line(int fd);
+
+#endif
